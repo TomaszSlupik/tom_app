a5713ccdb0270b6b325e4434da60c17a8688dba4
Tomasz Słupik <slupiktomasz@gmail.com>
2025-03-11 10:16:19 +0100

    Stworzenie projektu w Angular oraz utworzenie menu

---
+++ frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    document.body.classList.toggle('open', this.menuOpen);
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+    document.body.classList.remove('open');
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.menu') && !target.closest('.burger')) {
+      this.closeMenu();
+    }
+  }
+}
