f2004ec84895d65763ff0300e3543277d82c3e31
Tomasz Słupik <slupiktomasz@gmail.com>
2025-03-13 10:22:50 +0100

    Routing do podstron

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: "o_mnie", component: AboutusComponent},
+  {path: 'umiejetnosci', component: SkillsComponent},
+  {path: 'kontakt', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
