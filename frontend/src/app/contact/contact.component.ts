efcb970bb8a6ce2219a4ead54513b56c49be72ec
Tomasz Słupik <slupiktomasz@gmail.com>
2025-04-17 09:05:50 +0200

    Walidacja do formularza wysyłkowego

--- frontend/src/app/contact/contact.component.ts
+++ frontend/src/app/contact/contact.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  contactForm!: FormGroup;
+  formSubmitted: boolean = false;
 
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    console.log('Klik'); 
+    this.formSubmitted = true;
+    // wszystkie pola jako "touched", aby wymusić wyświetlenie błędów
+    this.contactForm.markAllAsTouched();
+
+    if (this.contactForm.invalid) {
+      // Formularz zawiera błędy – nie wysyłam dalej
+      return;
+    }
+
+    this.router.navigate(['/message-sent']);
+  }
 }
