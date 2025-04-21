ae234fba8edfe797ef4c592e5f57c99e099b44fd
Tomasz Słupik <slupiktomasz@gmail.com>
2025-04-21 17:07:11 +0200

    Routing do podstron

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { AboutusComponent } from './aboutus/aboutus.component';
 import { SkillsComponent } from './skills/skills.component';
 import { EducationComponent } from './education/education.component';
 import { ContactComponent } from './contact/contact.component';
+import { MessageSentComponent } from './message-sent/message-sent.component';
 
 
 const routes: Routes = [
   {path: "o_mnie", component: AboutusComponent},
   {path: 'umiejetnosci', component: SkillsComponent},
   {path: 'wyksztalcenie', component: EducationComponent},
-  {path: 'kontakt', component: ContactComponent}
+  {path: 'kontakt', component: ContactComponent},
+  { path: 'message-sent', component: MessageSentComponent}
 ];
 
 @NgModule({
